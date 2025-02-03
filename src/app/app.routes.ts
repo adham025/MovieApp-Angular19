5e146909d1217c6eff6e2c1acabb4b7f3177d6c1
adham025 <adhamgalal70@gmail.com>
2025-02-04 01:32:39 +0200

    Project Structure Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesListComponent,
+    title: 'Home Page'
+  },
+  {
+    path: 'movies/:id',
+    component: MovieDetailsComponent,
+    title: 'Movie Details'
+  },
+  {
+    path: 'wishlist',
+    component: WishlistComponent,
+    title: 'Wishlist Page'
+  },
+  {
+    path: 'search/:searchKey',
+    component: MovieSearchComponent,
+    title: 'Search'
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+    title: 'Registration'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Not Found'
+  }
+
+];
+
