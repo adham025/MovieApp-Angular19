09bdb80661d53089a9ad48df65ec402a9fb66c0e
adham025 <adhamgalal70@gmail.com>
2025-02-08 16:52:47 +0200

    New commit

--- src/app/home-page/movies-list/movies-list.component.ts
+++ src/app/home-page/movies-list/movies-list.component.ts
@@ -11,6 +11,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css'],
 })
+
 export class MoviesListComponent {
   movies: any[] = [];
   totalPages = 0;
