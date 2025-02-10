273d322b4223999326230c28dbe651c63be2e1e4
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-02-10 15:32:32 +0200

    final updates

---
+++ src/app/home-page/hero-section/hero-section.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { SearchServiceService } from '../../service/search-service.service';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [FormsModule],
+
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.css'
+})
+
+export class HeroSectionComponent {
+  searchKey: string = '';
+
+  constructor(private router: Router) {}
+
+  searchMovies() {
+    if (!this.searchKey.trim()) {
+      alert('Please enter a movie.');
+    } else {
+      this.router.navigate(['/search', this.searchKey]);
+    }
+  }
+}
